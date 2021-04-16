fc3f4f4bedca027ad773450e60c2bad43d54a4e6
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-04-16 14:10:30 -0300

    Update Project
    Update Project.

---
+++ android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
@@ -0,0 +1,22 @@
+package com.everis.bootcamp.threading
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
+class AstrosRepository {
+
+    fun loadData(): List<AstrosPeople>{
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://api.open-notify.org/astros.json")
+            .build()
+
+        val response = client.newCall(request).execute()
+        val result = parseJsonToResult(response.body?.string())
+        return  result.people
+    }
+
+    fun parseJsonToResult(json: String?) = Gson().fromJson(json, AstrosResult::class.java)
+}
---
+++ android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.everis.bootcamp.threading
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_load_data.setOnClickListener {
+            launchAstrosTask()
+        }
+    }
+
+    fun showData(list: List<AstrosPeople>?) {
+        textView_data.text = ""
+        list?.forEach { people ->
+            textView_data.append("${people.name} - ${people.craft}\n\n")
+        }
+    }
+
+    fun showLoadingIndicator() {
+        progressbar_load_indicator.visibility = View.VISIBLE
+    }
+
+    fun hideLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.GONE
+    }
+
+    fun launchAstrosTask(){
+        val task = TaskAstros()
+        task.execute()
+    }
+
+
+    inner class TaskAstros() : AsyncTask<Void, Int, List<AstrosPeople>>() {
+        private val repository = AstrosRepository()
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showLoadingIndicator()
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+        }
+
+        override fun doInBackground(vararg params: Void?): List<AstrosPeople> {
+            onProgressUpdate()
+            return  repository.loadData()
+        }
+
+        override fun onPostExecute(result: List<AstrosPeople>?) {
+            super.onPostExecute(result)
+            hideLoadingIndicator()
+            showData(result)
+        }
+    }
+
+}
